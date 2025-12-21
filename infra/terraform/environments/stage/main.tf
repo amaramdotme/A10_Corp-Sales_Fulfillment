9c8356a2ee9894f06d02144117661e2466553e5b
amaramdotme <amaramdotme@gmail.com>
2025-12-21 18:00:41 -0500

    fix: change stage SQL location to eastus2

--- infra/terraform/environments/stage/main.tf
+++ infra/terraform/environments/stage/main.tf
@@ -30,13 +30,13 @@ module "sql" {
   server_name         = "sql-a10corp-sales-stage"
   database_name       = "db-sales-fulfillment"
   resource_group_name = "rg-a10corp-sales-stage"
-  location            = "eastus"
+  location            = "eastus2" # Using eastus2 to avoid eastus restrictions
   
   admin_username      = "sqladmin"
   admin_password      = "P@ssw0rd123456!" # Note: Use variables/keyvault in production
 
   aks_subnet_id       = module.aks.node_subnet_id
-  sku_name            = "S0" # Using S0 to avoid eastus restrictions
+  sku_name            = "S0" 
 
   tags = {
     Environment = "Stage"
