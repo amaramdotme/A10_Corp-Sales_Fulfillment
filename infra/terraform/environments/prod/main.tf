639e4553140a07d38f8f820873196d2520bfe8d3
amaramdotme <amaramdotme@gmail.com>
2025-12-21 20:42:16 -0500

    ci: enable full automation for Stage and Prod with consistent identity and cross-region SQL fixes

--- infra/terraform/environments/prod/main.tf
+++ infra/terraform/environments/prod/main.tf
@@ -10,7 +10,7 @@ module "aks" {
   subnet_name         = "snet-a10corp-sales-prod-aks-nodes"
   identity_name       = "id-a10corp-sales-prod"
 
-  # Standard ACR and NSG fixes
+  # Fixes for ACR access and NSG rules
   acr_id              = "/subscriptions/fdb297a9-2ece-469c-808d-a8227259f6e8/resourceGroups/rg-root-iac/providers/Microsoft.ContainerRegistry/registries/acra10corpsales"
   node_nsg_name       = "nsg-a10corp-sales-prod-aks-nodes"
 
@@ -28,7 +28,7 @@ module "aks" {
 module "sql" {
   source = "../../modules/azure-sql"
 
-  server_name         = "sql-a10corp-sales-prod-v2"
+  server_name         = "sql-a10sales-prod-v2"
   database_name       = "db-sales-fulfillment"
   resource_group_name = "rg-a10corp-sales-prod"
   location            = "eastus2" 
@@ -37,7 +37,11 @@ module "sql" {
   admin_password      = "P@ssw0rdProd789!" # Note: Use variables/keyvault in production
 
   aks_subnet_id       = module.aks.node_subnet_id
-  sku_name            = "S0" # Standard tier for Prod
+  sku_name            = "S0" 
+
+  # Cross-region fix
+  enable_vnet_rule     = false
+  allow_azure_services = true
 
   tags = {
     Environment = "Prod"
